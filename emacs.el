2fa1e944aa78994330c25c34af5444243df92497
sKabYY <skabyy@gmail.com>
2013-12-01 11:56:45 +0800

    .

--- emacs.el
+++ emacs.el
@@ -66,6 +66,8 @@
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done t)
+(add-hook 'org-mode-hook
+          (lambda () (setq truncate-lines nil)))
 
 ; paredit
 (autoload 'enable-paredit-mode "paredit"
